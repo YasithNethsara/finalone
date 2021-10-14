3ef2f3f06ce595a8fa794343b9468a53cef87f73
Yasith Nethsara <89378001+YasithNethsara@users.noreply.github.com>
2021-10-14 12:42:39 +0530

    comment & change

--- fashionstore/fashionstore/src/main/java/com/fashionstore/repository/UserRepository.java
+++ fashionstore/fashionstore/src/main/java/com/fashionstore/repository/UserRepository.java
@@ -14,11 +14,12 @@ import com.fashionstore.domain.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+	//query sql search function get id & display id & feedback
 	@Query("SELECT u FROM User u WHERE u.id LIKE ?1")
 	public List<User> findAll(Long keyword);
 	
 	
-	//JRE file take a JPAREPOSITORY , its take to user entity  & primary key. Automatically generate
+	//JRE file take a JPA REPOSITORY , its take to user entity  & primary key. Automatically generate
 
 	
 }
